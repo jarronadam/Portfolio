23ee7abfecc74a3ad4bc61efe0fdb8f208b31385
Jarrondadam <Jarrondouglas@gmail.com>
2023-12-05 15:42:25 -0500

    websiteV1.1

--- src/App.js
+++ src/App.js
@@ -4,44 +4,52 @@ import resume from '../src/assets/myResume.pdf'
 import { AiFillGithub } from 'react-icons/ai'
 import { LuNewspaper } from 'react-icons/lu'
 import { BsLinkedin } from 'react-icons/bs'
+import { gif } from './assets/f666ce7b21152768e565ce0b0a3ce1ca.gif'
 
 function App() {
   return (
-
     <div class="App">
-      <header className="App-header">
+      <nav id="navbar">
+        <ul>
+          <li><a href="#home">Home</a></li>
+        </ul>
+      </nav>
+      <body>
+      <div className="App-header"></div>
+      <div class ="content">
         <img src={me} className="App-logo" alt="logo" />
         <p className='shadow-text'>
-           Douglas<br></br>
-          Welcome To My Homepage!
+          Jarron Douglas<br></br>
+          Welcome To My Portfolio!
         </p>
         <p className='shadow-text'>
-        Im excited to show you all my work as a upcoming computer science graduate and to put together a space for my work to be displayed and for those with similar interest to collaborate on future projects! <br></br><br></br>I am currently a senior attending the College of New Jersey with strong interest in both cyber security and Software engineering. <br></br>Thank for visit and I hope to hear from you soon! 
-      </p>
-      <h1 className='straight'>
-        <a
-          className="App-link"
-          href="https://www.linkedin.com/in/jarron-douglas-7665571ab/"
-          rel="stylesheet"
-        >
-          <BsLinkedin size={40} />
-        </a>
-        <a
-          className="App-link"
-          href={resume}
-          rel="stylesheet"
-        >
-          <LuNewspaper size={40} />
-        </a>
-        <a
-          className="App-link"
-          href="https://github.com/jarronadam"
-          rel="stylesheet"
-        >
-          <AiFillGithub size={40} />
-        </a>
-      </h1>
-    </header>
+          Im excited to show you all my work as a upcoming computer science graduate and to put together a space for my work to be displayed and for those with similar interest to collaborate on future projects! <br></br><br></br>I am currently a senior attending the College of New Jersey with strong interest in both cyber security and Software engineering. <br></br>Thank for visit and I hope to hear from you soon!
+        </p>
+        <h2 className='straight'>
+          <a
+            className="App-link"
+            href="https://www.linkedin.com/in/jarron-douglas-7665571ab/"
+            rel="stylesheet"
+          >
+            <BsLinkedin size={40} />
+          </a>
+          <a
+            className="App-link"
+            href={resume}
+            rel="stylesheet"
+          >
+            <LuNewspaper size={40} />
+          </a>
+          <a
+            className="App-link"
+            href="https://github.com/jarronadam"
+            rel="stylesheet"
+          >
+            <AiFillGithub size={40} />
+          </a>
+        </h2>
+        </div>
+      </body>
     </div >
   );
 }
--- src/index.js
+++ src/index.js
@@ -11,6 +11,30 @@ root.render(
   </React.StrictMode>
 );
 
+document.addEventListener('DOMContentLoaded', function() {
+  var xPos = 0;
+  var yPos = 0;
+  var xDirection = 1; // 1 for right, -1 for left
+  var yDirection = 1; // 1 for down, -1 for up
+
+  function moveBackground() {
+      xPos += xDirection;
+      yPos += yDirection;
+      
+      if (xPos > window.innerWidth || xPos < 0) {
+          xDirection *= -1;
+      }
+      if (yPos > window.innerHeight || yPos < 0) {
+          yDirection *= -1;
+      }
+
+      document.getElementById('gifBackground').style.backgroundPosition = xPos + 'px ' + yPos + 'px';
+      requestAnimationFrame(moveBackground);
+  }
+
+  moveBackground();
+});
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
